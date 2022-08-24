0428458f24766c2f73efd53f4a39ad16167d99a6
AspiringLowlife <billh890@gmail.com>
2022-08-24 15:19:24 +0200

    added somestuff

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,25 +4,17 @@ import '../custom.css';
 
 export default function Home() {
     return (
-        <div id="home">
-            {/* left column containing 
-            categories dropdown and 2 images */}
-            <div id="col-left">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
-            </div>
+        <div className="body">
+            <div id="home">
+                {/* left column containing 
+                categories dropdown and 2 images */}
+                <div id="col-left">
 
-            {/* right colum with banner, text, three images of products */}
-            <div id="col-right">
-                <h1>Banner</h1>
+                </div>
+                {/* right colum with banner, text, three images of products */}
+                <div id="col-right">
+                    <h1>Banner</h1>
+                </div>
             </div>
         </div>
     )
