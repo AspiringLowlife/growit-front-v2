23b2759f424541127ad64f58c062e76ad04cace7
AspiringLowlife <billh890@gmail.com>
2022-08-25 19:36:43 +0200

    yep

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import '../custom.css';
 import pic from '../Images/ots1.jpg'
 import pic1 from '../Images/Pots.jpg'
 import banner from '../Images/plantBanner.jpg'
+import { Dropdown } from "react-bootstrap";
 
 export default function Home() {
     return (
@@ -11,24 +12,25 @@ export default function Home() {
                 {/* left column containing 
                 categories dropdown and 2 images */}
                 <div id="col-left">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                            Categories
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/#">Plant</Dropdown.Item>
+                            <Dropdown.Item href="/#">Soil</Dropdown.Item>
+                            <Dropdown.Item href="/#">Tools</Dropdown.Item>
+                            <Dropdown.Item href="/#">Pots</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                     <div id="img-col">
-                    <img src={pic} width="250" height="300"/><br/>
-                    <img src={pic1} width="250" height="300"/>
-                    </div>                    
+                        <img src={pic} width="250" height="300" /><br />
+                        <img src={pic1} width="250" height="300" />
+                    </div>
                 </div>
                 {/* right colum with banner, text, three images of products */}
                 <div id="col-right">
-                    <img class="banner" src={banner} width="1000"/>
+                    <img class="banner" src={banner} width="1000" />
                 </div>
             </div>
         </div>
