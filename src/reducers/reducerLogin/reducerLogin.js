ddb8f23911f31d06121742175cc6ef60877499ad
BuildTools <unconfigured@null.spigotmc.org>
2022-09-09 22:10:09 +0200

    cleanup

--- src/reducers/reducerLogin/reducerLogin.js
+++ src/reducers/reducerLogin/reducerLogin.js
@@ -1,5 +1,4 @@
 import { cloneDeep } from 'lodash';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import AxiosService from '../../API/AxiosService';
