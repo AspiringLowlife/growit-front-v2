7334b8055dac1d7bf8780abfa5af9beea0da1ecf
BuildTools <unconfigured@null.spigotmc.org>
2022-09-17 03:33:09 +0200

    added Nice table library with lots of features

--- src/Pages/MaintainProfile.js
+++ src/Pages/MaintainProfile.js
@@ -1,7 +1,8 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { Component, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import AxiosService from "../API/AxiosService";
 import '../custom.css';
+import MaterialReactTable from 'material-react-table';
 
 export default function MaintainProfile() {
 
@@ -9,17 +10,17 @@ export default function MaintainProfile() {
     const [userDetails, updateUserDetails] = useState({})
     const [orders, setOrders] = useState([]);
 
-    async function getUserDetails() {        
+    async function getUserDetails() {
         const response = await AxiosService.getUserDetails(username)
         return response
     }
-    async function getUserOrders(){
+    async function getUserOrders() {
         await AxiosService.GetUserOrders(username)
-        .then(function (response){
-            setOrders(response.data)
-        })
+            .then(function (response) {
+                setOrders(response.data)
+            })
     }
-        
+
     useEffect(() => {
         getUserDetails().then((response) => {
             updateUserDetails(response.data)
@@ -28,7 +29,16 @@ export default function MaintainProfile() {
         // getting user orders
         getUserOrders()
     }, [])
-    
+
+
+    // Showcasing table things you can do
+    // see documentation for features: https://www.material-react-table.com/docs/examples/basic
+    const tableInstanceRef = useRef(null);
+
+    function santizeDate(date) {
+        return date.substring(0, 10)
+    }
+
     return (
         <div class="container">
             <div className="col-group">
@@ -40,15 +50,67 @@ export default function MaintainProfile() {
                     {ProfileBox("Password", "*******")}
                     {ProfileBox("Delivery Details", userDetails?.address)}
                 </div>
-                <div className="my-col">
-                    <div className="row"><h1>Past Orders</h1></div>
-                    {orders.map((order) => {
-                        return (
-                            OrderBox(order.ordersID, order.date_Started)
-                        )
-                    })}
-                    {OrderBox("#1324", "07/12/2022")}
-                </div>
+                <h1>Your Orders</h1>
+                <MaterialReactTable
+                    columns={[
+                        {
+                            accessorKey: 'ordersID', //simple recommended way to define a column
+                            header: 'Order ID',
+                            muiTableHeadCellProps: { sx: { color: 'green' } }, //custom props
+                        },
+                        {
+                            accessorKey: 'date_Started', //simple recommended way to define a column
+                            header: 'Date Started',
+                            muiTableHeadCellProps: { sx: { color: 'green' } }, //custom props
+                            Cell: ({ cell }) => {
+                                return <div>{santizeDate(cell.getValue())}</div>
+                            },
+                        },
+                    ]}
+                    data={orders}
+                    renderDetailPanel={({ row }) => (
+                        <MaterialReactTable
+                            columns={[
+                                {
+                                    accessorKey: 'itemID', //simple recommended way to define a column
+                                    header: 'Item ID',
+                                    muiTableHeadCellProps: { sx: { color: 'green' } }, //custom props
+                                },
+                                {
+                                    accessorKey: 'item_Name', //simple recommended way to define a column
+                                    header: 'Item Name',
+                                    muiTableHeadCellProps: { sx: { color: 'green' } }, //custom props
+                                },
+                                {
+                                    accessorKey: 'price', //simple recommended way to define a column
+                                    header: 'Price',
+                                    muiTableHeadCellProps: { sx: { color: 'green' } }, //custom props
+                                    Footer: () => (<strong>Total: R {row.original.itemsInOrder.reduce((accumulator, object) => accumulator + object.price, 0)}</strong>)
+                                },
+                                {
+                                    accessorKey: 'quantity', //simple recommended way to define a column
+                                    header: 'Quantity',
+                                    muiTableHeadCellProps: { sx: { color: 'green' } }, //custom props
+                                },
+                            ]}
+                            data={row.original.itemsInOrder}
+
+                            //turn off a feature like so
+                            enableRowSelection={false}
+                            enableColumnOrdering={false}
+                            enableGlobalFilter={false}
+                            renderTopToolbar={false}
+                        />
+
+                    )}
+                    tableInstanceRef={tableInstanceRef}
+
+                    //turn off a feature like so
+                    enableRowSelection={false}
+                    enableColumnOrdering={false}
+                    enableGlobalFilter={false}
+                    renderTopToolbar={false}
+                />
             </div>
         </div>
     )
@@ -61,14 +123,14 @@ function ProfileBox(title, info) {
         <div className="my-row border border-success ">
             <h5>{title}:</h5>
             <p>{info}</p>
-            <a id="link"className="sit-right" href="#">Edit</a>
+            <a id="link" className="sit-right" href="#">Edit</a>
         </div>
     )
 }
 function OrderBox(OrderNum, Date) {
     return (
         <div className="my-row border border-success">
-            <span>Order No: {OrderNum}</span>            
+            <span>Order No: {OrderNum}</span>
             <span>Date Completed: {Date}</span>
             <a id="link" style={{ paddingLeft: 0 }}>Expand</a>
         </div>
